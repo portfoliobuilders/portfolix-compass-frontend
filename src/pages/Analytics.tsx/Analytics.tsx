77077730ed885d9d435dac09e13a07d65150af61
portfoliobuilders <portfoliobuilders.ind@gmail.com>
2025-12-02 16:40:35 +0530

    feat: Add Page 5/8 - Analytics.tsx

---
+++ src/pages/Analytics.tsx/Analytics.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const Analytics: React.FC = () => (<div className="p-6"><h1 className="text-3xl font-bold text-gray-900">Analytics</h1></div>);
+export default Analytics;
