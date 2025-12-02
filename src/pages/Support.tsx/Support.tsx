43ced805dca28862d92e936f185a4c4bb011e3a2
portfoliobuilders <portfoliobuilders.ind@gmail.com>
2025-12-02 18:55:26 +0530

    feat: Add Support page template (7/8)

---
+++ src/pages/Support.tsx/Support.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Support: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-gray-900 mb-4">Support & Help</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">Support and help page coming soon...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
